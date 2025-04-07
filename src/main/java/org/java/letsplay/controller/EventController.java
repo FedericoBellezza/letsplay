5bdf49838adaaf87a769c6c38ae05fe8f5de3110
Federico Bellezza <federico.bellezza2001@gmail.com>
2025-04-07 16:27:00 +0930

    switch pc

--- src/main/java/org/java/letsplay/controller/EventController.java
+++ src/main/java/org/java/letsplay/controller/EventController.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
