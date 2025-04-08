06b737bf2d0223cec39b1285d94dd625b654f0d4
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-04-08 14:20:49 +0200

    FIX: little style fix

--- src/main/java/org/java/letsplay/controller/EventController.java
+++ src/main/java/org/java/letsplay/controller/EventController.java
@@ -5,7 +5,6 @@ import org.java.letsplay.model.Event;
 import org.java.letsplay.service.CategoryService;
 import org.java.letsplay.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
